12602d1bb7e30331790238164524f4ab08ed406c
Wasim GULISTAN <wgulista@e1r2p16.42.fr>
2016-04-12 04:09:06 +0200

    modif

--- get_next_line.c
+++ get_next_line.c
@@ -6,58 +6,33 @@
 /*   By: wgulista <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/05 12:25:22 by wgulista          #+#    #+#             */
-/*   Updated: 2016/03/05 12:25:24 by wgulista         ###   ########.fr       */
+/*   Updated: 2016/03/21 12:25:23 by wgulista         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char     *add_to_line(char *line, int cur, char *buff, int *start)
+int					get_next_line(int fd, char **line)
 {
-	char          *new_elem;
-  	int           old_len;
- 
-  	old_len = (line) ? ft_strlen(line) : 0;
-  	new_elem = ft_memalloc((old_len + cur + 1) * sizeof(*new_elem));
-  	ft_strncpy(new_elem, line ? line : "", old_len);
-  	ft_strncpy(new_elem + old_len, buff + *start, cur);
-  	new_elem[old_len + cur] = 0;
-  	if (line)
-    		free(line);
-  	*start += cur + 1;
-  	return (new_elem);
-}
- 
-int            get_next_line(const int fd, char **line)
-{
-  	static char   buff[BUFF_SIZE + 1];
- 	static int    in_buf = 0;
- 	static int    start;
-  	int           cur;
+	static char		*rest;
+	char			buff[BUFF_SIZE + 1];
+	int				rd;
 
-  	if (fd <= 0 || line == NULL)
-  		return (-1);
-  	*line = 0;
-  	cur = 0;
-  	while (1)
-    {
-      	if (start >= in_buf)
-        {
-          	start = 0;
-          	if (!(in_buf = read(fd, buff, BUFF_SIZE)))
-            	return (-1);
-        	if (in_buf == -1)
-          		return (-1);
-        	cur = 0;
-        }
-      	if (buff[start + cur] == '\n')
-      	{
-      		*line = add_to_line(*line, cur, buff, &start);
-      		return (1);
-      	}
-      	if (start + cur == in_buf - 1)
-        	*line = add_to_line(*line, cur + 1, buff, &start);
-      	cur++;
-    }
-    return (0);
+	if (fd <= 0 || line == NULL || BUFF_SIZE < 1)
+		return (-1);
+	rest = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	*line = (char*)malloc(sizeof(char) * (ft_strlen(rest) + BUFF_SIZE + 1));
+	while ((rd = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[rd] = '\0';
+		ft_strncat(*line, buff, ft_strlen(buff));
+	}
+	if (rd == 0 && (rest = ft_strchr(*line, '\n')))
+	{
+		*rest = '\0';
+		return (1);
+	}
+	if (rd == -1)
+		return (-1);
+	return (0);
 }
