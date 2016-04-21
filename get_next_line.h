e5a6dfd713f5521e84008507f8c3774531629079
gitWasimGulistan <gitWasimGulistan@users.noreply.github.com>
2016-04-21 15:51:45 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -15,6 +15,15 @@
 # include "libft/includes/libft.h"
 
 # define BUFF_SIZE 1
-int		get_next_line(int const fd, char **line);
+
+typedef struct		s_gnl
+{
+	char			*save;
+	char			*buff;
+	char			*tmp;
+	int				rd;
+}					t_gnl;
+
+int					get_next_line(const int fd, char **line);
 
 #endif
