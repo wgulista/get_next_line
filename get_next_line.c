ac7c6a7a3fa6aeeb506b556468e521352a5d8266
gitWasimGulistan <gitWasimGulistan@users.noreply.github.com>
2016-04-21 12:03:56 +0200

    42filecheck OK sauf fuite

--- get_next_line.c
+++ get_next_line.c
@@ -12,60 +12,58 @@
 
 #include "get_next_line.h"
 
-int		line_len(char *str, int c)
+static int		line_len(char *save)
 {
 	int				i;
 
 	i = 0;
-	while (str[i] != c)
+	while (save[i] != '\n' && save[i] != '\0')
 		i++;
-	if (str[i] == c)
-		return (i);
-	return (0);
+	return (i);
 }
 
-int		check_line(char **save, char *tmp, char **line)
+static int		check_line_and_eof(char **save, char *tmp)
 {
 	if (ft_strchr(*save, '\n'))
 	{
-		*line = ft_strsub(*save, 0, line_len(*save, '\n'));
-		tmp = ft_strdup(ft_strchr(*save, '\n') + 1);
-		free(*save);
-		*save = tmp;
+		tmp = *save;
+		*save = ft_strdup(ft_strchr(*save, '\n') + 1);
+		ft_strdel(&tmp);
 		return (1);
 	}
-	if (ft_strcmp(*save, '\0') != 0)
+	if (line_len(*save) > 0)
 	{
-		*line = ft_strsub(*save, 0, line_len(*save, '\0'));
-		free(*save);
+		*save += line_len(*save);
 		return (1);
 	}
 	return (0);
 }
 
-int		get_next_line(int const fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
 	static char		*save[256];
-	char			buff[BUFF_SIZE + 1];
+	char			*buff;
 	char			*tmp;
 	int				rd;
 
-	if (fd < 0 || fd > 256|| line == NULL || BUFF_SIZE < 0)
+	if (fd < 0 || fd > 256 || line == NULL || BUFF_SIZE < 0 ||
+		!(buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
 	if (!save[fd])
 		save[fd] = ft_strdup("");
-	while ((rd = read(fd, buff, BUFF_SIZE)))
+	while (!(ft_strchr(save[fd], '\n')) &&
+		(rd = read(fd, buff, BUFF_SIZE)) > 0 && rd != 0)
 	{
 		buff[rd] = '\0';
-		tmp = ft_strjoin(save[fd], buff);
-		free(save[fd]);
-		save[fd] = tmp;
-		if (*save[fd] == '\0' || ft_strchr(save[fd], '\n'))
-			break ;
+		tmp = save[fd];
+		save[fd] = ft_strjoin(save[fd], buff);
+		ft_strdel(&tmp);
 	}
+	ft_strdel(&buff);
 	if (rd == -1 || save[fd] == NULL)
 		return (-1);
-	if (check_line(&save[fd], tmp, line) == 1)
+	*line = ft_strsub(save[fd], 0, line_len(save[fd]));
+	if (check_line_and_eof(&save[fd], tmp) == 1)
 		return (1);
 	return (rd ? 1 : 0);
 }
