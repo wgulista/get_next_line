ff04bd4ef2c3c21263bcd8945a72860b6160670f
gitWasimGulistan <gitWasimGulistan@users.noreply.github.com>
2016-04-21 15:50:50 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,58 +12,78 @@
 
 #include "get_next_line.h"
 
-static int		line_len(char *save)
+static int		ft_check_len(char *save)
 {
 	int				i;
 
 	i = 0;
+	if (save == NULL)
+		return (-1);
 	while (save[i] != '\n' && save[i] != '\0')
 		i++;
 	return (i);
 }
 
-static int		check_line_and_eof(char **save, char *tmp)
+static int		check_line(t_gnl *gnl)
 {
-	if (ft_strchr(*save, '\n'))
+	if (ft_strchr(gnl->save, '\n'))
 	{
-		tmp = *save;
-		*save = ft_strdup(ft_strchr(*save, '\n') + 1);
-		ft_strdel(&tmp);
-		return (1);
-	}
-	if (line_len(*save) > 0)
-	{
-		*save += line_len(*save);
+		gnl->tmp = ft_strdup(ft_strchr(gnl->save, '\n') + 1);
+		ft_strdel(&gnl->save);
+		gnl->save = ft_strdup(gnl->tmp);
+		ft_strdel(&gnl->tmp);
 		return (1);
 	}
 	return (0);
 }
 
-int				get_next_line(const int fd, char **line)
+static int		check_eof(t_gnl *gnl, char **line)
 {
-	static char		*save[256];
-	char			*buff;
-	char			*tmp;
-	int				rd;
-
-	if (fd < 0 || fd > 256 || line == NULL || BUFF_SIZE < 0 ||
-		!(buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-		return (-1);
-	if (!save[fd])
-		save[fd] = ft_strdup("");
-	while (!(ft_strchr(save[fd], '\n')) &&
-		(rd = read(fd, buff, BUFF_SIZE)) > 0 && rd != 0)
+	if (ft_check_len(gnl->save) > 0)
 	{
-		buff[rd] = '\0';
-		tmp = save[fd];
-		save[fd] = ft_strjoin(save[fd], buff);
-		ft_strdel(&tmp);
-	}
-	ft_strdel(&buff);
-	if (rd == -1 || save[fd] == NULL)
-		return (-1);
-	*line = ft_strsub(save[fd], 0, line_len(save[fd]));
-	if (check_line_and_eof(&save[fd], tmp) == 1)
+		*line = ft_strdup(gnl->save);
+		ft_strdel(&gnl->save);
 		return (1);
-	return (rd ? 1 : 0);
+	}
+	ft_strdel(&gnl->save);
+	return (0);
+}
+
+static void		ft_read_fd(t_gnl *gnl, const int fd)
+{
+	if (!gnl->save)
+		gnl->save = ft_strnew(1);
+	while (!(ft_strchr(gnl->save, '\n')) &&
+		(gnl->rd = read(fd, gnl->buff, BUFF_SIZE)) > 0 && gnl->rd != 0)
+	{
+		gnl->buff[gnl->rd] = '\0';
+		gnl->tmp = ft_strjoin(gnl->save, gnl->buff);
+		ft_strdel(&gnl->save);
+		gnl->save = ft_strdup(gnl->tmp);
+		ft_strdel(&gnl->tmp);
+		if (ft_strchr(gnl->save, '\n'))
+			break ;
+	}
+	ft_strdel(&gnl->buff);
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	static t_gnl	gnl;
+
+	if (fd < 0 || !(line) ||
+		!(gnl.buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+		return (-1);
+	ft_read_fd(&gnl, fd);
+	if (gnl.rd == -1 || gnl.save == NULL)
+	{
+		ft_strdel(&gnl.save);
+		return (-1);
+	}
+	*line = ft_strsub(gnl.save, 0, ft_check_len(gnl.save));
+	if (check_line(&gnl) == 1)
+		return (1);
+	if (gnl.rd == 0 && ft_strchr(gnl.save, '\n') == NULL)
+		return (check_eof(&gnl, line));
+	return (0);
 }
